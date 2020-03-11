5cfefc2c4c1bc428dd7810186b519a1067e2503b
Markov Sergey <markov@qtech.ru>
2020-03-11 10:48:27 +0300

    правит helyfly

--- helyfly/js/script.js
+++ helyfly/js/script.js
@@ -1,69 +1,38 @@
 /* eslint-disable */
-'use strict';
+'use strict'; // polyfill swiper for IE11
 
-// polyfill swiper for IE11
 if (!String.prototype.startsWith) {
   Object.defineProperty(String.prototype, 'startsWith', {
-      value: function(search, rawPos) {
-          var pos = rawPos > 0 ? rawPos|0 : 0;
-          return this.substring(pos, pos + search.length) === search;
-      }
+    value: function value(search, rawPos) {
+      var pos = rawPos > 0 ? rawPos | 0 : 0;
+      return this.substring(pos, pos + search.length) === search;
+    }
   });
 }
-
 /* Слайдер для блока tab */
+
+
 var gallerySwiper = new Swiper('.gallery__container', {
   slidesPerView: 'auto',
   spaceBetween: 20,
-  loop: true,
+  loop: true
 });
-
 /* Слайдер для блока map */
+
 var mapSwiper = new Swiper('.map__slider-swiper-container', {
   slidesPerView: 1,
   spaceBetween: 10,
   loop: true,
   navigation: {
     nextEl: '.map__slider-swiper-button-next',
-    prevEl: '.map__slider-swiper-button-prev',
+    prevEl: '.map__slider-swiper-button-prev'
   },
   pagination: {
-    el: '.map__slider-swiper-pagination',
-  },
+    el: '.map__slider-swiper-pagination'
+  }
 });
-
-
-/* Yandex карта */
-
-ymaps.ready(init);
-function init(){
-  // Создание карты.
-  var myMap = new ymaps.Map("map", {
-      // Координаты центра карты.
-      // Порядок по умолчанию: «широта, долгота».
-      // Чтобы не определять координаты центра карты вручную,
-      // воспользуйтесь инструментом Определение координат.
-      center: [55.75897861, 37.61587440],
-      // Уровень масштабирования. Допустимые значения:
-      // от 0 (весь мир) до 19.
-      zoom: 13,
-      controls: [],
-      behaviors: ['drag']
-  });
-
-  var myPlacemark = new ymaps.Placemark([55.75897861, 37.61587440], 
-    {balloonContent: 'Встречаемся тут!'}, 
-    {preset: "islands#redIcon"}
-  );
-
-  // myPin.add(myPlacemark);
-  myMap.geoObjects.add(myPlacemark);
-
-  // Балун откроется в точке «привязки» балуна — т. е. над меткой.
-  // myMap.balloon.open();
-};
-
 /* модальное окно */
+
 var modal = document.querySelector('.modal');
 var body = document.querySelector('body');
 var heroBtn = document.querySelector('.hero__btn');
@@ -71,24 +40,23 @@ var overlay = document.querySelector('.overlay');
 var close = document.querySelector('.modal__close');
 var modalInput = document.querySelector('.modal [type="text"]');
 var esc = 27;
-
 /* открытие модального окна */
+
 heroBtn.addEventListener('click', function (e) {
   e.preventDefault();
 
   if (modal.classList.contains('visually-hidden')) {
     modal.classList.remove('visually-hidden');
     overlay.classList.remove('visually-hidden');
-    body.style.overflow = 'hidden';
-    // modalInput.focus();
+    body.style.overflow = 'hidden'; // modalInput.focus();
   } else {
     modal.classList.add('visually-hidden');
     overlay.classList.add('visually-hidden');
     body.style.overflow = 'auto';
   }
 });
-
 /* закрытие модального окна */
+
 var closeModal = function closeModal(e) {
   modal.classList.add('visually-hidden');
   overlay.classList.add('visually-hidden');
@@ -98,111 +66,99 @@ var closeModal = function closeModal(e) {
 close.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 window.addEventListener('keydown', function (e) {
-  console.log(e)
+  console.log(e);
+
   if (e.code === 'Escape' || e.keyCode === esc) {
     closeModal();
   }
 });
-
 /* маска формы телефона */
+
 $("#tel").mask("8(999) 999-9999");
 $("#date").mask("99.99.9999");
 $("#card").mask("99999-9999-9999-9999");
 $("#date-card").mask("99.9999");
 $("#cvv").mask("999");
-
-
 /* видео youtube */
-let video = document.querySelector('.desc__video');
-let videoImg = document.querySelector('.desc__video-img');
 
-video.addEventListener('click', () => {
-  let iframe = createIframe();
-
+var video = document.querySelector('.desc__video');
+var videoImg = document.querySelector('.desc__video picture');
+video.addEventListener('click', function () {
+  var iframe = createIframe();
   videoImg.remove();
   video.appendChild(iframe);
 });
-
 /* cоздаем iframe */
+
 function createIframe() {
-  let iframe = document.createElement('iframe');
-  
-	iframe.setAttribute('src', 'https://www.youtube.com/embed/4JS70KB9GS0?autoplay=1');
-	iframe.setAttribute('allowfullscreen', '');
-	iframe.setAttribute('allow', 'autoplay');
-
-	return iframe;
+  var iframe = document.createElement('iframe');
+  iframe.setAttribute('src', 'https://www.youtube.com/embed/4JS70KB9GS0?autoplay=1');
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
+  return iframe;
 }
-
-
 /* cоздаем линк еще для блока Отзывы */
-let linkMore = document.querySelector('.review__link');
-let block = document.querySelector('.review__list');
 
-linkMore.addEventListener('click', (e) => {
+
+var linkMore = document.querySelector('.review__link');
+var block = document.querySelector('.review__list');
+linkMore.addEventListener('click', function (e) {
   e.preventDefault();
-  block.insertAdjacentHTML('beforeend', 
-    `
-    <!-- Отзыв -->
-    <div class="review__item">
-
-      <div class="review__img">
-        <img src="./img/antonova.png" alt="Валерия Антонова">
-      </div>
-
-      <p class="review__title-name">Валерия Антонова</p>
-      <p class="review__title-prof">Арт-директор</p>
-      <div class="review__title-wrap">
-        <div class="review__title-rating">
-          <img src="./img/rating.svg" alt="Рейтинг">
-        </div>
-        <p class="review__extra">Супер! Я бы заказала съемку снова!</p>
-      </div>
-
-      <p class="review__text">Хотим выразить огромную благодарность вашей компании! Получили море ощущений, все прошло просто замечательно. Фотки получились великолепные, отдельное спасибо фотографу!</p>
-      <p class="review__data">27 Апреля, 2019г.</p>
-    </div>
-    `
-  );
+  block.insertAdjacentHTML('beforeend', "\n    <!-- \u041E\u0442\u0437\u044B\u0432 -->\n\t  <div class=\"review__item\">\n      <div class=\"review__img\">\n        <picture>\n          <source srcset=\"./img/antonova.webp 1x, ./img/antonova@2x.webp 2x\" type=\"image/webp\">\n          <img src=\"./img/antonova.png\" srcset=\"./img/antonova@2x.png 2x\" alt=\"\u0412\u0430\u043B\u0435\u0440\u0438\u044F \u0410\u043D\u0442\u043E\u043D\u043E\u0432\u0430\">\n        </picture>\n      </div>\n      <p class=\"review__title-name\">\u0412\u0430\u043B\u0435\u0440\u0438\u044F \u0410\u043D\u0442\u043E\u043D\u043E\u0432\u0430</p>\n      <p class=\"review__title-prof\">\u0410\u0440\u0442-\u0434\u0438\u0440\u0435\u043A\u0442\u043E\u0440</p>\n      <div class=\"review__title-wrap\">\n        <div class=\"review__title-rating\">\n          <img src=\"./img/rating.svg\" alt=\"\u0420\u0435\u0439\u0442\u0438\u043D\u0433\">\n        </div>\n        <p class=\"review__extra\">\u0421\u0443\u043F\u0435\u0440! \u042F \u0431\u044B \u0437\u0430\u043A\u0430\u0437\u0430\u043B\u0430 \u0441\u044A\u0435\u043C\u043A\u0443 \u0441\u043D\u043E\u0432\u0430!</p>\n      </div>\n      <p class=\"review__text\">\u0425\u043E\u0442\u0438\u043C \u0432\u044B\u0440\u0430\u0437\u0438\u0442\u044C \u043E\u0433\u0440\u043E\u043C\u043D\u0443\u044E \u0431\u043B\u0430\u0433\u043E\u0434\u0430\u0440\u043D\u043E\u0441\u0442\u044C \u0432\u0430\u0448\u0435\u0439 \u043A\u043E\u043C\u043F\u0430\u043D\u0438\u0438! \u041F\u043E\u043B\u0443\u0447\u0438\u043B\u0438 \u043C\u043E\u0440\u0435 \u043E\u0449\u0443\u0449\u0435\u043D\u0438\u0439, \u0432\u0441\u0435 \u043F\u0440\u043E\u0448\u043B\u043E \u043F\u0440\u043E\u0441\u0442\u043E \u0437\u0430\u043C\u0435\u0447\u0430\u0442\u0435\u043B\u044C\u043D\u043E. \u0424\u043E\u0442\u043A\u0438 \u043F\u043E\u043B\u0443\u0447\u0438\u043B\u0438\u0441\u044C \u0432\u0435\u043B\u0438\u043A\u043E\u043B\u0435\u043F\u043D\u044B\u0435, \u043E\u0442\u0434\u0435\u043B\u044C\u043D\u043E\u0435 \u0441\u043F\u0430\u0441\u0438\u0431\u043E \u0444\u043E\u0442\u043E\u0433\u0440\u0430\u0444\u0443!</p>\n      <p class=\"review__data\">27 \u0410\u043F\u0440\u0435\u043B\u044F, 2019\u0433.</p>\n    </div>\n    ");
 });
-
 /* cоздает линк еще для блока about__leader - Отзывы */
-let aboutLeaderLink = document.querySelector('.about__leader-link');
-let aboutLeader = document.querySelector('.about__leader');
 
-aboutLeaderLink.addEventListener('click', (e) => {
+var aboutLeaderLink = document.querySelector('.about__leader-link');
+var aboutLeader = document.querySelector('.about__leader');
+aboutLeaderLink.addEventListener('click', function (e) {
   e.preventDefault();
-  aboutLeader.insertAdjacentHTML('beforeend', 
-    `
-    <p>Сначала мы (я и моя команда) изучаем интересы клиентов, а затем придумываем подходящее решение, которое соответствует бюджету и планам клиента. Мы никогда не зависим от субпоставщиков и не свяжемся.</p>
-    `
-  );
+  aboutLeader.insertAdjacentHTML('beforeend', "\n    <p>\u0421\u043D\u0430\u0447\u0430\u043B\u0430 \u043C\u044B (\u044F \u0438 \u043C\u043E\u044F \u043A\u043E\u043C\u0430\u043D\u0434\u0430) \u0438\u0437\u0443\u0447\u0430\u0435\u043C \u0438\u043D\u0442\u0435\u0440\u0435\u0441\u044B \u043A\u043B\u0438\u0435\u043D\u0442\u043E\u0432, \u0430 \u0437\u0430\u0442\u0435\u043C \u043F\u0440\u0438\u0434\u0443\u043C\u044B\u0432\u0430\u0435\u043C \u043F\u043E\u0434\u0445\u043E\u0434\u044F\u0449\u0435\u0435 \u0440\u0435\u0448\u0435\u043D\u0438\u0435, \u043A\u043E\u0442\u043E\u0440\u043E\u0435 \u0441\u043E\u043E\u0442\u0432\u0435\u0442\u0441\u0442\u0432\u0443\u0435\u0442 \u0431\u044E\u0434\u0436\u0435\u0442\u0443 \u0438 \u043F\u043B\u0430\u043D\u0430\u043C \u043A\u043B\u0438\u0435\u043D\u0442\u0430. \u041C\u044B \u043D\u0438\u043A\u043E\u0433\u0434\u0430 \u043D\u0435 \u0437\u0430\u0432\u0438\u0441\u0438\u043C \u043E\u0442 \u0441\u0443\u0431\u043F\u043E\u0441\u0442\u0430\u0432\u0449\u0438\u043A\u043E\u0432 \u0438 \u043D\u0435 \u0441\u0432\u044F\u0436\u0435\u043C\u0441\u044F.</p>\n    ");
   aboutLeaderLink.remove();
 });
-
 /* cоздает линк еще для блока about__services - Отзывы */
-let aboutServices = document.querySelector('.about__services');
-let aboutServicesLink = document.querySelector('.about__services-link');
 
-aboutServicesLink.addEventListener('click', (e) => {
+var aboutServices = document.querySelector('.about__services');
+var aboutServicesLink = document.querySelector('.about__services-link');
+aboutServicesLink.addEventListener('click', function (e) {
   e.preventDefault();
-  aboutServices.insertAdjacentHTML('beforeend', 
-    `
-    <p>Качественный подбор места и локации для вашей съемки. При необходимости всегда возможно прерваться на обед/ужин. Более 50 фотографий на выходе с обработкой и замечательные впечатления о Дубае !</p>
-    `
-  );
+  aboutServices.insertAdjacentHTML('beforeend', "\n    <p>\u041A\u0430\u0447\u0435\u0441\u0442\u0432\u0435\u043D\u043D\u044B\u0439 \u043F\u043E\u0434\u0431\u043E\u0440 \u043C\u0435\u0441\u0442\u0430 \u0438 \u043B\u043E\u043A\u0430\u0446\u0438\u0438 \u0434\u043B\u044F \u0432\u0430\u0448\u0435\u0439 \u0441\u044A\u0435\u043C\u043A\u0438. \u041F\u0440\u0438 \u043D\u0435\u043E\u0431\u0445\u043E\u0434\u0438\u043C\u043E\u0441\u0442\u0438 \u0432\u0441\u0435\u0433\u0434\u0430 \u0432\u043E\u0437\u043C\u043E\u0436\u043D\u043E \u043F\u0440\u0435\u0440\u0432\u0430\u0442\u044C\u0441\u044F \u043D\u0430 \u043E\u0431\u0435\u0434/\u0443\u0436\u0438\u043D. \u0411\u043E\u043B\u0435\u0435 50 \u0444\u043E\u0442\u043E\u0433\u0440\u0430\u0444\u0438\u0439 \u043D\u0430 \u0432\u044B\u0445\u043E\u0434\u0435 \u0441 \u043E\u0431\u0440\u0430\u0431\u043E\u0442\u043A\u043E\u0439 \u0438 \u0437\u0430\u043C\u0435\u0447\u0430\u0442\u0435\u043B\u044C\u043D\u044B\u0435 \u0432\u043F\u0435\u0447\u0430\u0442\u043B\u0435\u043D\u0438\u044F \u043E \u0414\u0443\u0431\u0430\u0435 !</p>\n    ");
   aboutServicesLink.remove();
 });
-
 /* cоздает дополнительные фото для юлока gallery */
-let galleryWrap = document.querySelector('.gallery__wrap');
-let galleryMore = document.querySelector('.gallery__more');
 
-
-galleryMore.addEventListener('click', (e) => {
+var galleryWrap = document.querySelector('.gallery__wrap');
+var galleryMore = document.querySelector('.gallery__more');
+galleryMore.addEventListener('click', function (e) {
   e.preventDefault();
-  let galleryWrapClone = galleryWrap.cloneNode(true); // клонировать сообщение
-  console.log(galleryWrapClone)
+  var galleryWrapClone = galleryWrap.cloneNode(true); // клонировать сообщение
+
+  console.log(galleryWrapClone);
   galleryWrap.insertAdjacentElement('beforeend', galleryWrapClone);
   galleryMore.remove();
 });
+/* Yandex карта */
+
+ymaps.ready(init);
+
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [55.75897861, 37.61587440],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 13,
+    controls: [],
+    behaviors: ['drag']
+  });
+  var myPlacemark = new ymaps.Placemark([55.75897861, 37.61587440], {
+    balloonContent: 'Встречаемся тут!'
+  }, {
+    preset: "islands#redIcon"
+  }); // myPin.add(myPlacemark);
+
+  myMap.geoObjects.add(myPlacemark);
+};
